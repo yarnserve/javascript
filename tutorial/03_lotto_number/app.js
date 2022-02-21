e8a249fb5dd1bd940b0e7aa5aa8046a7924eeefc
yarnserve <yarnserve@gmail.com>
2022-02-21 12:23:10 +0900

    gallery-app

--- tutorial/03_lotto_number/app.js
+++ tutorial/03_lotto_number/app.js
@@ -2,9 +2,41 @@ const numbers = document.querySelector(".numbers");
 const start = document.querySelector(".start");
 const reset = document.querySelector(".reset");
 const list = document.querySelector(".list");
+const dimm = document.querySelector(".dimm");
 
 let lottoNumbers = [];
 let lottoList = [];
+let count = 10;
+
+function checkGame() {
+  count--;
+  if (count <= 0) {
+    dimm.classList.add("active");
+    snackBar();
+  }
+}
+
+function snackBar() {
+  const snackbar = new SnackBar();
+  snackbar.make(
+    "message",
+    [
+      "안녕하세요",
+      "",
+      // vertical position
+      "bottom",
+      // horizontal position
+      "center",
+    ],
+    null
+  );
+
+  snackbar.action(function () {
+    snackbar.close();
+    dimm.classList.remove("active");
+    resetGame();
+  });
+}
 
 function addList() {
   list.innerHTML = "";
@@ -12,8 +44,13 @@ function addList() {
 
   lottoList.forEach((item) => {
     const p = document.createElement("p");
-    console.log(item);
-    p.innerText = item;
+
+    item.forEach((num) => {
+      const span = document.createElement("span");
+      span.textContent = num;
+      p.appendChild(span);
+    });
+
     list.prepend(p);
   });
 }
@@ -31,13 +68,18 @@ function startGame() {
   lottoNumbers = [];
   while (lottoNumbers.length < 6) {
     const num = Math.floor(Math.random() * 45) + 1;
-    lottoNumbers.push(num);
+
+    if (lottoNumbers.indexOf(num) === -1) {
+      lottoNumbers.push(num);
+    }
   }
   paintNumber();
   addList();
+  checkGame();
 }
 
 function resetGame() {
+  count = 10;
   lottoNumbers = [];
   lottoList = [];
   list.innerHTML = "";
