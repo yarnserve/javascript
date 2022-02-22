c61ced0cff405008d8622f812251e63704f475ac
yarnserve <yarnserve@gmail.com>
2022-02-22 12:15:30 +0900

    weather js

--- tutorial/06_todo_app/js/weather.js
+++ tutorial/06_todo_app/js/weather.js
@@ -3,11 +3,29 @@
 
 const weather = document.querySelector(".weather");
 
+const API_KEY = "26a8f813a56d73684d04cdff5a1f7720";
+
+async function getWeather(lat, lon) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const res = await fetch(url);
+  const data = await res.json();
+  weather.textContent = `${data.main.temp}℃, ${data.name}`;
+}
+
+function saveCoords(coords) {
+  localStorage.setItem("coords", JSON.stringify(coords));
+  getWeather(coords.latitude, coords.longitude);
+}
+
 function geoSucces(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
-  console.log(latitude);
-  console.log(longitude);
+
+  const coords = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coords);
 }
 
 function geoError() {
@@ -18,8 +36,17 @@ function askCoords() {
   navigator.geolocation.getCurrentPosition(geoSucces, geoError);
 }
 
+function loadCoords() {
+  const COORDS = JSON.parse(localStorage.getItem("coords"));
+  getWeather(COORDS.latitude, COORDS.longitude);
+}
+
 function init() {
-  askCoords();
+  if (localStorage.getItem("coords")) {
+    loadCoords();
+  } else {
+    askCoords();
+  }
 }
 
 init();
